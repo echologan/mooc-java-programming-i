c0b890ff7849bf0fa12be85cb46c54505671385f
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-04 19:14:57 -0800

    first commit, working on java for cs classes

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,12 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+
+        System.out.println("Welcome to this programming course");
+
+
+
+    }
+}
