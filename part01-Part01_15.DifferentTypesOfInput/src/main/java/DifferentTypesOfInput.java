7f538a16ec4cf1b11ee5f92cf33772ae293896e6
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-13 15:48:25 -0800

    Putting all of it together string, int, double, and boolean.

--- part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
+++ part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -8,5 +8,20 @@ public class DifferentTypesOfInput {
 
         // Write your program here
 
+        System.out.println("Give a string:");
+        String text = scan.nextLine();
+        System.out.println("Give an integer:");
+        int wholeNumber = Integer.valueOf(scan.nextLine());
+        System.out.println("Give a double:");
+        double anyNum = Double.valueOf(scan.nextLine());
+        System.out.println("Give a boolean:");
+        boolean trueOrFalse = Boolean.valueOf(scan.nextLine());
+        System.out.println("You gave the string " + text);
+        System.out.println("You gave the integer " + wholeNumber);
+        System.out.println("You gave the double " + anyNum);
+        System.out.println("You gave the boolean " + trueOrFalse);
+
+
+
     }
 }
