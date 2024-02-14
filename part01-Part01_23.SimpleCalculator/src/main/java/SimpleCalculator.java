bb79326576cbd5c2b3894c35facfc36678b049da
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-14 11:07:42 -0800

    wasnt passing at first because 1.0 wasnt inside the parenthesis on the last Sout.

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -8,5 +8,20 @@ public class SimpleCalculator {
 
         // Write your program here
 
+        System.out.println("Give the first number:");
+        int firstInput = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int secondInput = Integer.valueOf(scanner.nextLine());
+
+        System.out.println(firstInput + " + " + secondInput + " = " + (firstInput+secondInput));
+
+        System.out.println(firstInput + " - " + secondInput + " = " + (firstInput-secondInput));
+
+        System.out.println(firstInput + " * " + secondInput + " = " + (firstInput * secondInput));
+
+        System.out.println(firstInput + " / " + secondInput + " = " + (1.0 * firstInput/secondInput) );
+
+
     }
 }
