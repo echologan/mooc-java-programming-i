3ba86689d52b099ccc210c7112e4b184a24e684b
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-14 10:06:41 -0800

    finding the average of three numbers, using integer but then converting it to float by multiplying by decimal.

--- part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -8,5 +8,16 @@ public class AverageOfThreeNumbers {
 
         // Write your program here
 
+        System.out.println("Give the first number:");
+        int firstInput = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int secondInput = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int thirdInput = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("The average is " + 1.0 * (firstInput + secondInput + thirdInput) / 3);
+
     }
 }
