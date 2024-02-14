12b966edc926522a371da44d31797e3bf73a96e5
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-13 19:50:21 -0800

    using integer conversion from user to create the sum of two inputs.

--- part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
+++ part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
@@ -7,6 +7,11 @@ public class SumOfTwoNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give the first number: ");
+        int sum1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int sum2 = Integer.valueOf(scanner.nextLine());
+        int sum3 = sum1 + sum2;
+        System.out.println("The sum of the numbers is " + sum3);
     }
 }
