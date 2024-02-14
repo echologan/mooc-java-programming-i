6efa150044691b9e23ba3ffaa9dd9200a0e57766
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-14 14:53:03 -0800

    using conditional if statment, notice how scanner is now called scan.

--- part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -6,6 +6,12 @@ public class Orwell {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here
+        // Write your program 
+        
+        System.out.println("Give a number:");
+        int n1 = Integer.valueOf(scan.nextLine());
+            if (n1 == 1984) {
+                System.out.println("Orwell");
+            }
     }
 }
