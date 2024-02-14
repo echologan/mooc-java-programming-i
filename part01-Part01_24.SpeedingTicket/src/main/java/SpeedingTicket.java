ef9ede300fef88070e3dcd4cd4f66bb51d971d6b
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-14 13:38:53 -0800

    using simple conditional statment with if keyword followed by boolean, this example if speed is over 120 you get ticket.

--- part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
+++ part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -7,5 +7,11 @@ public class SpeedingTicket {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here. 
+        System.out.println("Give Speed:");
+        int speedInput = Integer.valueOf(scanner.nextLine());
+
+        if (speedInput > 120) {
+            System.out.println("Speeding ticket!");
+        }
     }
 }
