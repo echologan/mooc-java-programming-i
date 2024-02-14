a486576239a6a293162e7d72b210b980b168f11f
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-13 20:50:44 -0800

    getting the average of the input and learning the different ways to convert integers to floating-point numbers

--- part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -1,12 +1,23 @@
 
 import java.util.Scanner;
 
+import javax.sound.midi.Soundbank;
+
 public class AverageOfTwoNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+
+        System.out.println("Give the first number:");
+        int firstInput = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int secondInput = Integer.valueOf(scanner.nextLine());
+
+        double result = 1.0 * (firstInput + secondInput)/2;
+        System.out.println("The average is " + result);
 
     }
 }
