144cf0170c53650850c377cb9d4038242413df20
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-15 14:32:53 -0800

    using .equals with a string variable in the parameter of the if statement.

--- part01-Part01_33.Password/src/main/java/Password.java
+++ part01-Part01_33.Password/src/main/java/Password.java
@@ -7,5 +7,15 @@ public class Password {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here 
+
+        System.out.println("Password?");
+        
+        String pw = scan.nextLine();
+
+        if (pw.equals("Caput Draconis")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
