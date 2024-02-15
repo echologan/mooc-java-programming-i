5abea4ea32232a0483ea919c9fc0ebd5964f023b
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-15 14:40:08 -0800

    reinforcing the concept of conditional statements and comparing strings. we can compare strings not with == but insead equals command because strings can hold a limitless amount of characters whereas integers, floating, and boolean values always contain a single number or value.

--- part01-Part01_34.Same/src/main/java/Same.java
+++ part01-Part01_34.Same/src/main/java/Same.java
@@ -7,5 +7,19 @@ public class Same {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here. 
+
+        System.out.println("Enter the first string:");
+
+        String input1 = scan.nextLine();
+
+        System.out.println("Enter the second string:");
+
+        String input2 = scan.nextLine();
+
+        if (input1.equals(input2)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
     }
 }
