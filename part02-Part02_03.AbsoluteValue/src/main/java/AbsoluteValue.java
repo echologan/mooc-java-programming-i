0e372674671d02a2f9eda22a5f373328c304685c
loganjamestech <156109555+loganjamestech@users.noreply.github.com>
2024-02-20 18:48:42 -0800

    using conditional logic

---
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int usrInt = Integer.valueOf(scanner.nextLine());
+
+        if (usrInt < 0 ) {
+            System.out.println(usrInt * -1);
+        } else {
+            System.out.println(usrInt);
+        }
+
+    }
+}
