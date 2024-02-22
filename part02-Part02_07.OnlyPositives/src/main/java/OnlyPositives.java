02410df338f25809ca892fa615de3f16f49aae8e
luckylogan <loganjames448@gmail.com>
2024-02-22 15:04:05 -0800

    using while loop introducing continue keyword along with break if a certain condition was met.

--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,6 +6,24 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Give a number: ");
+            int numInput = Integer.valueOf(scanner.nextLine());
+            
+            if (numInput < 0) {
+                System.out.println("Unsuitable number");
+                continue;
+            }
+
+            if (numInput > 0) {
+                numInput = numInput * numInput;
+                System.out.println(numInput);
+            }
+
+            if (numInput == 0) {
+                break;
+            }
+        }
 
     }
 }
