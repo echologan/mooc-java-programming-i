0af2ca1efdd26326753842a61433f8bb19ab2d9b
luckylogan <loganjames448@gmail.com>
2024-02-22 08:50:19 -0800

    changing file back to normal

--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,7 +6,6 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("testing on new GH");
 
     }
 }
