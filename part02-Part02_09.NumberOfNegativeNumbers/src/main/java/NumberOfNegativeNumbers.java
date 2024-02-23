074ecab0c354f7a5f032de612519336afe56d9cf
luckylogan <loganjames448@gmail.com>
2024-02-22 19:46:47 -0800

    still using variable outside the loop to I can refernce it after the loop breaks. this time around im counting the negative numbers. for some reason im passing all the test but not getting points recieving error message that 'some test failed on the server' not sure.

--- part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -6,5 +6,23 @@ public class NumberOfNegativeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int negCount = 0;
+
+        while (true) {
+
+            System.out.println("Give a number: ");
+
+            int number = Integer.valueOf(scanner.nextLine());
+
+           if (number < 0) {
+            negCount = negCount + 1;
+           } 
+
+           if (number == 0) {
+            break;
+           }
+           
+        }
+        System.out.println("Number of negative numbers: " + negCount);
     }
 }
