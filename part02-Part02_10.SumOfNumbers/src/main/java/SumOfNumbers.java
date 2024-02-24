16feb94e36b6e28ac28167eabe61bca2d57c2cdf
luckylogan <loganjames448@gmail.com>
2024-02-23 16:42:46 -0800

    using propper indentation fixed my submission problems. also using if , else inside the while loop isntead a bunch of ifs. using scanner.nextInt instead of scanner.nextLine. add value to variable using += is useful aswell. putting sout 'sum of the number' inside the while loop solved my previous problems, before I had the sum outside the loop.

--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,18 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0; 
 
+        while(true) {
+            System.out.println("Give a number: ");
+            int num = scanner.nextInt();
+            
+            if (num == 0) {
+                System.out.println("Sum of the numbers: " + sum);
+                break;
+            } else {
+                sum += num;
+            } //end of if
+        } //end of while loop
     }
 }
