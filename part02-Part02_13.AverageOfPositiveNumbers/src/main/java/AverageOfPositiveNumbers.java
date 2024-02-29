cc97260451e47627a501489df4707a6e98b08667
luckylogan <loganjames448@gmail.com>
2024-02-28 18:18:00 -0800

    had to use if statement outside the while loop to figure out if the input was 0. If included inside while loop it would mess up my break instead of only saying it when theres only 0 not a bunch of numbers and then 0.

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,26 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int input, sum = 0, count = 0;
+
+        while (true) {
+            input = scanner.nextInt();
+
+            if (input == 0) {
+                break;
+            }
+
+            if (input > 0) {
+                sum += input;
+                count ++;
+            }
+
+        } // end while loop
+
+        if (input == 0) {
+            System.out.println("Cannot calculate the average");
+        }
+
+        System.out.println(1.0 * sum / count);
     }
 }
