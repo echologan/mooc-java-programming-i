357b952faab6254af045f9cceb9d774e06d3d71a
luckylogan <loganjames448@gmail.com>
2024-03-01 15:52:36 -0800

    using the for loop is really neccessary here, good exercise to show which statement does what in the for loop parathesis, first one is declaring the variable and assigning the starting value, second is the condition or where it will break without having to state break keyword, last or 3rd one is executed every time the code block has been executed.

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -7,5 +7,18 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.print("Where to? ");
+        int endNum = Integer.valueOf(scanner.nextLine());
+
+        System.out.print("Where from? ");
+        int startNum = Integer.valueOf(scanner.nextLine());
+        
+        for (int i = startNum; i <= endNum; i++) {
+            System.out.println(i);
+
+            if (startNum > endNum) {
+                break;
+            }
+        }
     }
 }
