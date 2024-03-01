4e0ede9b11889f198e31a2761a137d72f0a59ebb
luckylogan <loganjames448@gmail.com>
2024-02-29 20:04:04 -0800

    using c=input as a parameter of my for loop to get the input of the user via scanner, if set at 0 the loop will start at 0 every time instead of starting at what the input is.

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,11 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int input = Integer.valueOf(scanner.nextLine());
+
+        for (int c = input; c <= 100; c++) {
+            System.out.println(c);
+        }
+
     }
 }
