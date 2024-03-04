4a6ddb3103dc649bf446b808b0cbd57493743846
luckylogan <loganjames448@gmail.com>
2024-03-04 10:08:03 -0800

    i variable is increading by one every time the loop goes through, thus assigning an outside variable c (for count) will add its previous number to the container c.

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -5,6 +5,13 @@ public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Last number? ");    
+        int lastNum = Integer.valueOf(scanner.nextLine());
+        int c = 0;
+        for (int i = 0; i <= lastNum; i++) {
+            c += i;
+        }
+        System.out.println("The sum is " + c);
 
     }
 }
