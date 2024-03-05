8f251b495ef660c61dbfe5fa09c982bb16567712
luckylogan <loganjames448@gmail.com>
2024-03-04 18:31:15 -0800

    using for loop to manipulate where the loop starts and stops. += is useful which means variable = variable plus variable.

--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,14 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("First number? ");
+        int fNum = Integer.valueOf(scanner.nextLine());
+        System.out.print("Last number? ");
+        int sNum = Integer.valueOf(scanner.nextLine());
+        int c = 0;
+        for (int i = fNum; i <= sNum; i++) {
+            c += i;
+        }
+        System.out.println("The sum is " + c);
     }
 }
