cf27930bac93277be7fe4f9925b65120f6542834
luckylogan <loganjames448@gmail.com>
2024-03-07 18:37:36 -0800

    creating a method, printUntilNumber, with a parameter of int type with name number which runs a for loop. the for loop worked better than while loop for me, the while loop commented out will just run 4, 4 times instead of starting at 1, altough easy fix for loop seems for intuitive.

--- part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -3,7 +3,18 @@
 public class FromOneToParameter {
 
     public static void main(String[] args) {
-
+        printUntilNumber(4);
     }
 
+    public static void printUntilNumber(int number) {
+        for (int i = 1; i <= number; i++) {
+
+            System.out.println(i);
+        }
+        // int i = 0;
+        // while (i < number) {
+        //     System.out.println(number);
+        //     i++;
+        // }
+    }
 }
