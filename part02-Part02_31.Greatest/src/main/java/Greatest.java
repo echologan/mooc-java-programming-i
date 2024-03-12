6b75c216879b060910b1a3ddae6cc70facd3ba74
luckylogan <loganjames448@gmail.com>
2024-03-11 19:29:37 -0700

    solving for greatest number out of 3 numbers using method via if statments and returns.

--- part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -3,8 +3,19 @@ public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
         //write some code here
+        int greatest;
 
-        return -1;
+        if (number1 > number2) {
+            greatest = number1;
+        } else {
+            greatest = number2;
+        }
+
+        if (number3 > greatest) {
+            greatest = number3;
+        }
+
+        return greatest;
     }
 
     public static void main(String[] args) {
