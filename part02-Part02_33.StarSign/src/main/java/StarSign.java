e125b7c985dc62e58672c1595c95a169e74ccbe3
luckylogan <loganjames448@gmail.com>
2024-03-12 18:55:26 -0700

    example of the call stack and returning values from a method. need to understand parameters more

--- part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -20,6 +20,11 @@ public class StarSign {
 
     public static void printStars(int number) {
         // first part of the exercise
+        for(int i = 0; i < number; i++) {
+            System.out.println("*");
+        }
+
+        System.out.println("");
     }
 
     public static void printSquare(int size) {
