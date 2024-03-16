1eb295936bf648d0f9c9ab3e7e2d9c6e1ff1e6f7
luckylogan <loganjames448@gmail.com>
2024-03-15 18:06:21 -0700

    working with arrayList now and all the methods that are built in to it.

---
+++ part03-Part03_01.ThirdElement/src/main/java/ThirdElement.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ThirdElement {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+            list.add("Joe");
+            list.add("Emmy");
+            list.add("Roger");
+        }
+
+        System.out.println(list.get(2));
+    }
+}
