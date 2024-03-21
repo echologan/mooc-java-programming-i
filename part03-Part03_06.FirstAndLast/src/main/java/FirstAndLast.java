8e425611792ed5fe3f7fb008f1b2468009d56a6b
luckylogan <loganjames448@gmail.com>
2024-03-21 09:25:10 -0700

    using list.get(list.size()-1)); to find the last input works whereas last solution worked but wasnt right for this challenge.

--- part03-Part03_06.FirstAndLast/src/main/java/FirstAndLast.java
+++ part03-Part03_06.FirstAndLast/src/main/java/FirstAndLast.java
@@ -12,7 +12,7 @@ public class FirstAndLast {
             String input = scanner.nextLine();
             if (input.equals("")) {
                 System.out.println(list.get(0));
-                System.out.println(list.getLast());
+                System.out.println(list.get(list.size()-1));
                 break;
             }
 
