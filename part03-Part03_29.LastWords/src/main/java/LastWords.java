c741234ec6c98df8b821920418a9911a75925c69
luckylogan <loganjames448@gmail.com>
2024-04-08 14:20:17 -0700

    adding comments

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -13,6 +13,14 @@ public class LastWords {
             }
 
             // storing variable input which is the scanner input into array
+
+            /*
+             * we cant store input variable into array using split bc
+             * it splits the string into elements based on the spaces
+             * whereas not using split on input doesn't work because
+             * input is just one string or element, input reads a single line
+             * of input from the user
+             */
             String[] inputArray = input.split(" ");
             int getElementAtThisIndex = (inputArray.length - 1);
             System.out.println(inputArray[getElementAtThisIndex]);
