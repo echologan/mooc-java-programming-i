796141f2f75d82a75919722dc3e00c259baa8cea
luckylogan <loganjames448@gmail.com>
2024-04-08 13:28:55 -0700

    adding comments

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -8,18 +8,23 @@ public class AVClub {
 
         while (true) {
             String userInput = scanner.nextLine();
-            if(userInput.equals("")) {
+            // if scanner input is blank break while loop
+            if (userInput.equals("")) {
                 break;
             }
 
+            // string array capturing scanner input
+            // 'splitting' out spaced from the scanner input
             String[] arrayWords = userInput.split(" ");
 
-            for(String word : arrayWords) {
-                if(word.contains("av")) {
+            // for-each loop
+            // each element in String array will be called word
+            for (String word : arrayWords) {
+                // if element in array contains 'av' print the element
+                if (word.contains("av")) {
                     System.out.println(word);
-                }
-            }
-        }
-
-    }
-}
+                } // closing if
+            } // closing for
+        } // closing while
+    } // closing main
+} // closing class
