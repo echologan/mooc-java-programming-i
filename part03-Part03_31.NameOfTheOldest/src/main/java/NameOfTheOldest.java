0f11538bb2bcd7f8bad33023c3cc4ae07316b2e9
luckylogan <loganjames448@gmail.com>
2024-04-09 14:30:53 -0700

    adding comments

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -19,6 +19,19 @@ public class NameOfTheOldest {
             String[] array = input.split(",");
             age = Integer.valueOf(array[1]);
 
+            /*
+             * age is holding the second element of the array in a int value via
+             * calling the 1 index. each time this while loop runs and we get another input
+             * from a new line the age is still stored from last loop. thus we need another
+             * variable to compare against in this case 'n'. so if I run program and input
+             * logan,4 (enter, nextline) jake,6 (enter and enter again to break out) age
+             * first
+             * holds value 4, compares 4 to 'n' which starts out at 0 so now age and n is
+             * both 4.
+             * next loop age is then switched to next input which if now 6 so at this point
+             * n and age
+             * are different values.
+             */
             if (age > n) {
                 n = age;
                 name = String.valueOf(array[0]);
