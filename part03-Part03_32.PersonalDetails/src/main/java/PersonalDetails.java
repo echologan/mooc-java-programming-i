81a6a9247361bf63a1627411e7c175f27216a578
luckylogan <loganjames448@gmail.com>
2024-04-09 15:18:22 -0700

    This program reads the name and birth years from the user until an empty line is entered. The name and birth year are sepated by comma. After the program prints the longest name and the average of the birth years. If multiple names are equally longest, print any of them.

---
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PersonalDetails {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int nameLength = 0;
+        int longerName = 0;
+        String name = "filler";
+        int birthYear = 0;
+        int avgOfB = 0;
+        int loopCount = 0;
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] pieces = input.split(",");
+            nameLength = pieces[0].length();
+
+            if (nameLength > longerName) {
+                longerName = nameLength;
+                name = String.valueOf(pieces[0]);
+            }
+
+            birthYear = Integer.valueOf(pieces[1]);
+            avgOfB += birthYear;
+            loopCount++;
+
+        } // closing while
+
+        System.out.println("Longest name: " + name);
+        System.out.println("Average of the birth years: " + 1.0 * avgOfB / loopCount);
+    }
+}
