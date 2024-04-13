fe45a7b8d04fea49f42d9c3ca4f7bd25de2dfbc1
luckylogan <loganjames448@gmail.com>
2024-04-12 17:50:46 -0700

    adding in comments

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -15,20 +15,26 @@ public class PersonalDetails {
         int avgOfB = 0;
         int loopCount = 0;
 
+        // each time something is entered the loop starts again unless nothing input
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("")) {
                 break;
             }
 
+            // string array by splitting string variable
             String[] pieces = input.split(",");
+            // find how many letters in array index 0 (name) using length method
             nameLength = pieces[0].length();
 
+            // longerName is place holder for the highest number each time loops
             if (nameLength > longerName) {
                 longerName = nameLength;
+                // if the name is longer capture that actual value or name in index 0
                 name = String.valueOf(pieces[0]);
             }
 
+            // getting the value of index 1 (birth year), converting it to integer
             birthYear = Integer.valueOf(pieces[1]);
             avgOfB += birthYear;
             loopCount++;
