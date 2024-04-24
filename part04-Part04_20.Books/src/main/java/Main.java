94f3287c493c07598aa9e8d2329186d168444fbf
logan-james <loganjames448@gmail.com>
2024-04-23 17:52:48 -0700

    Testing move

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        
-        // implement here the program that allows the user to enter 
-        // book information and to examine them
 
+        // implement here the program that allows the user to enter
+        // book information and to examine them
+        // testing
     }
 }
